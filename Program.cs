e95027e6f80442e94d90f0e2697a65fc6b5f3a85
SyrillDex <syrilldexl@gmail.com>
2024-11-05 14:41:35 +0800

    Convert the calculator functions to a method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace MyProject
 {
@@ -7,96 +8,45 @@ namespace MyProject
         static void Main(string[] args)
         {
 
-            Random rnd = new Random();
-            string player;
-            string computer;
-            bool playAgain = true;
 
-            while (playAgain)
+            string op = "";
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            calculator(num1, num2, op);
+            
+        }
+
+        static void calculator(double num1, double num2, string op)
+        {
+
+            while (op != "+" && op != "-" && op != "*" && op != "/")
             {
-                player = "";
-                computer = "";
-
-                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
-                {
-                    Console.Write("Please Choose ROCK, PAPER, or SCISSORS: ");
-                    player = Console.ReadLine().ToUpper();
-                    Console.WriteLine();
-                }
-                switch(rnd.Next(1,4))
-                {
-                    case 1:
-                        computer = "ROCK";
-                        break;
-                    case 2:
-                        computer = "PAPER";
-                        break;
-                    case 3:
-                        computer = "SCISSORS";
-                        break;
-                }
-                Console.WriteLine("Player: " + player);
-                Console.WriteLine("Computer: " + computer);
-                switch (player)
-                {
-                    case "ROCK":
-                        if (computer == "ROCK")
-                        {
-                            Console.WriteLine("Its a TIE");
-                        }
-                        else if(computer == "PAPER")
-                        {
-                            Console.WriteLine("YOU LOSE");
-                        }
-                        else
-                        {
-                            Console.WriteLine("YOU WIN");
-                        }
-                        break;
-                    case "PAPER":
-                        if (computer == "ROCK")
-                        {
-                            Console.WriteLine("YOU WIN");
-                        }
-                        else if (computer == "PAPER")
-                        {
-                            Console.WriteLine("Its a TIE");
-                        }
-                        else
-                        {
-                            Console.WriteLine("YOU LOSE");
-                        }
-                        break;
-                    case "SCISSORS":
-                        if (computer == "ROCK")
-                        {
-                            Console.WriteLine("YOU LOSE");
-                        }
-                        else if (computer == "PAPER")
-                        {
-                            Console.WriteLine("YOU WIN");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Its a TIE");
-                        }
-                        break;
-                }
+                Console.Write("Choose if +, - , *, or /: ");
+                op = Console.ReadLine();
                 Console.WriteLine();
-                Console.WriteLine("Would you like to play again? (Y/N)");
-                string response = Console.ReadLine().ToUpper();
-                Console.WriteLine();
-
-                if(response == "Y")
-                {
-                    playAgain = true;
-                }
-                else
-                {
-                    playAgain = false;
-                }
             }
-            Console.WriteLine("Thanks for Playing");
+
+            Console.Write("The answer is: ");
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+            }
         }
     }
 }
